c618cc8ac77f31ba038356a0ca4bef2be5fd91cb
Alberto Gómez <albertogomrod5@gmail.com>
2023-02-15 17:49:49 +0100

    Iteración 2

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,27 +1,56 @@
-const express = require("express")
-const router = express.Router()
-const User = require("../models/User.model.js")
-const bcrypt = require("bcryptjs")
-
+const express = require("express");
+const router = express.Router();
+const User = require("../models/User.model.js");
+const bcrypt = require("bcryptjs");
 
 router.get("/signup", (req, res, next) => {
-    res.render("auth/signup-form.hbs")
-})
+  res.render("auth/signup-form.hbs");
+});
 
 router.post("/signup", async (req, res, next) => {
-    
-    try {
-        // Encriptar la contraseña
-        const salt = await bcrypt.genSalt(10);
-        const hashPassword = await bcrypt.hash(req.body.password, salt);
-        await User.create({
-            username: req.body.username,
-            password: hashPassword
-        })
-        res.redirect("/")
-    } catch (error) {
-        next(error);
+  try {
+    // Encriptar la contraseña
+    const salt = await bcrypt.genSalt(10);
+    const hashPassword = await bcrypt.hash(req.body.password, salt);
+    await User.create({
+      username: req.body.username,
+      password: hashPassword,
+    });
+    res.redirect("/");
+  } catch (error) {
+    next(error);
+  }
+});
+
+//GET ("/auth/login")
+router.get("/login", (req, res, next) => {
+  res.render("auth/login-form.hbs");
+});
+
+//POST ("/auth/login")
+router.post("/login", async (req, res, next) => {
+  try {
+    const foundUser = await User.findOne({ username: req.body.username });
+    if (foundUser === null) {
+      res.render("auth/login-form.hbs", {
+        errorMessage: "Ese usuario no existe.",
+      });
+      return;
     }
-})
+    const correctPassword = await bcrypt.compare(
+      req.body.password,
+      foundUser.password
+    );
+
+    if (correctPassword === false) {
+        res.render ("auth/login-form.hbs",{
+            errorMessage: "Contraseña incorrecta"
+        })
+        return
+    }
+  } catch (error) {
+    next(error);
+  }
+});
 
 module.exports = router;
